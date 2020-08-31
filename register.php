59eeb41638daabc42c7e44c54d36d87f2e6c3dc5
jgrimaldos <albertogrimal2@hotmail.com>
2020-08-31 03:50:11 -0500

    envio mail

--- register.php
+++ register.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
 
   <meta charset="utf-8">
@@ -8,7 +7,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-
 
   <title>UEB Registro</title>
 
@@ -110,6 +108,7 @@ body {
     color: #fff;
   }
 </style>
+
 <!-- FUNCIONES -->
 <script>
   window.onload=function(){
@@ -296,6 +295,7 @@ body {
   }
 
 </script>
+
 <body>
 
   <div class="container">
--- ws/registerUser.php
+++ ws/registerUser.php
@@ -1,5 +1,53 @@
 <?php
 include_once('../persistencia/db.php');
+ require '../mailer/PHPMailer.php';
+ require '../mailer/SMTP.php';
+
+ require '../mailer/Exception.php';
+
+function enviarCorreo($mail, $name)
+{
+    $PHPmail=new PHPMailer();
+    $PHPmail->CharSet = 'UTF-8';
+    $PHPmail->IsSMTP();
+    $PHPmail->Host       = 'smtp.gmail.com';
+    $PHPmail->SMTPSecure = 'tls';
+
+    $PHPmail->Port       = 587;
+    $PHPmail->SMTPDebug  = 0;
+    $PHPmail->SMTPAuth   = true;
+    $PHPmail->Username   = 'practicas.uelbosque@gmail.com';
+    $PHPmail->Password   = 'PracticasUEB123';
+    $PHPmail->SetFrom('practicas.uelbosque@gmail.com', "Practicas UEB");
+    $PHPmail->Subject    = 'Confirma tu correo electrónico';
+    
+    $PHPmail->AddEmbeddedImage("..\assets\images\logos\logoMail.jpg", "logo");
+    $PHPmail->MsgHTML('<div style="background-color: rgba(222,222,222,0.6); margin-left: 15%; margin-right: 15%;">
+                        <div style="text-align: center;">
+                        <img src=\'cid:logo\' alt="Universidad El Bosque" style=" max-width: 100%; max-height: 100%;
+                        pointer-events: none;
+                        cursor: default;">
+                        </div>
+                        <div style="margin-left: 10%; margin-right: 10%;">
+                        <br>
+                        <p>Te damos la bienvenida '.$name.' al portal de practicas de la Universidad El Bosque.</p>
+                        <p>Para poder usar todos nuestros servicios por favor haz click en el siguiente boton para terminar tu proceso de registro.</p>
+                        <br><br>
+                        </div>
+                        </div>');
+
+    $PHPmail->AddAddress($mail, $name);
+        
+    if(!$PHPmail->Send())
+    {
+    echo "Error sending: " . $PHPmail->ErrorInfo;
+    }
+    else
+    {
+    echo "E-mail sent";
+    }
+}
+
 $mail=$_POST['mail'];
 $name=$_POST['name'];
 $pass=$_POST['pass'];
@@ -20,6 +68,7 @@ if (!$mysqli->query($sql)) {
         );
     }
 }else{
+    enviarCorreo($mail, $name);
     $response = array(
         'comment' => 'Se agregó satisfactoriamente',
         'status' => true
@@ -31,3 +80,4 @@ $mysqli->close();
 echo json_encode($response);
 
 ?>
+

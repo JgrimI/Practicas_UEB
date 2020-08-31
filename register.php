69d82a264be9166cfecf1c33ecf4547d7ed2400f
jleonf2020 <61398900+jleonf2020@users.noreply.github.com>
2020-08-30 19:47:15 -0500

    Registro empresas

--- register.php
+++ register.php
@@ -15,6 +15,7 @@
   <!-- Custom fonts for this template-->
   <link href="estilos_tp2/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+  <link href="assets/plugins/dropify/css/dropify.min.css" rel="stylesheet">
 
   <!-- Custom styles for this template-->
   <link href="estilos_tp2/css/sb-admin-2.min.css" rel="stylesheet">
@@ -28,8 +29,11 @@
   <!-- Custom scripts for all pages-->
   <script src="estilos_tp2/js/sb-admin-2.min.js"></script>
   <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
+  <link rel="stylesheet" href="assets/dist/css/dropify.min.css">
+
   <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+  <script src="assets/dist/js/dropify.min.js"></script>
 
 </head>
 
@@ -108,7 +112,17 @@ body {
 </style>
 <!-- FUNCIONES -->
 <script>
-  window.onload=getPrograms();
+  window.onload=function(){
+    dropify = $('.dropify').dropify({
+      messages: {
+        'default': 'Arrastra el archivo o haz click aqui',
+        'replace': 'Arrastra o clikea para remplazar',
+        'remove':  'Quitar',
+        'error':   'Ooops, algo a salido mal.'
+    }
+    });
+    getPrograms();
+  };
   function getPrograms(){
     $.ajax({
         type: "POST",
@@ -130,17 +144,27 @@ body {
         },
     })
   }
+
   function formAtStud(){
     $('#form_estudiante').css('display','block');
     $('#form_bienvenida').css('display','none');
+    $('#form_empresa').css('display','none');
   }
   function formAtWelcome(){
     $('#form_estudiante').css('display','none');
     $('#f_est')[0].reset();
+    $('#f_emp')[0].reset();
     $('#alert_name').css('display','none');
     $('#alert_pw').css('display','none');
     $('#alert_mail').css('display','none');
     $('#form_bienvenida').css('display','block');
+    $('#form_empresa').css('display','none');
+
+  }
+  function formAtCompany(){
+    $('#form_empresa').css('display','block');
+    $('#form_bienvenida').css('display','none');
+    $('#form_estudiante').css('display','none');
   }
   function verifyPass(){
     var pass=document.getElementById('pass').value;
@@ -153,6 +177,20 @@ body {
       $('#alert_pw').css('display','block');
     }else{
       $('#alert_pw').css('display','none');
+    }
+    return false;
+  }
+  function verifyPassCp(){
+    var pass=document.getElementById('passCp').value;
+    var verify=document.getElementById('verifyCp').value;
+    if(pass==verify && pass!='' && verify!=''){
+      $('#alert_pwCp').css('display','none');
+      return true;
+    }
+    else if(pass!=verify && pass!='' && verify!=''){
+      $('#alert_pwCp').css('display','block');
+    }else{
+      $('#alert_pwCp').css('display','none');
     }
     return false;
   }
@@ -216,9 +254,45 @@ body {
             console.log(data);
         },
     });
-      
     }
-    
+  }
+
+  function regCompany(){
+    if(verifyPassCp()){
+      $.ajax({
+        type: "POST",
+        url: "ws/registerCompany.php",
+        data:new FormData($('#f_emp')[0]),
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function (data) {
+            console.log(data);
+            data = JSON.parse(data);
+            if (data["status"] == 1) {
+              $('.dropify-clear').click();
+              Swal.fire(
+								  'Bien hecho!',
+								  'Se han enviado a tu correo las credenciales, activa tu cuenta y disfruta de la plataforma!!!',
+								  'success'
+								).then(function(){
+                  window.location='index.php';
+                })
+            }else{
+              if(data['error']=1062){
+                Swal.fire(
+								  'Error!',
+								  'Ya se encuentra registrado en la plataforma!!!',
+								  'error'
+								)
+              }
+            }
+        },
+        error: function (data) {
+            console.log(data);
+        },
+    });
+    }
   }
 
 </script>
@@ -231,6 +305,8 @@ body {
 
       <div class="card w-75 black-text" style="margin-top:6%;padding-bottom: 2%;">
         <div class="card-body" id="body_form" name="body_form">
+
+          <!-- Este es el formulario que se despliega cuando se pulsa estudiante -->
           <div id="form_estudiante" name="form_estudiante" style="display:none;">
             <a href="javascript:void(0);" class="btn btn-success" style="float:left;" onclick="formAtWelcome();">Volver</a>
             <form id="f_est" action="javascript:void(0);" onsubmit="reg();">
@@ -238,12 +314,12 @@ body {
               <div class="alert alert-danger mb-0" role="alert" id="alert_name" style="display:none;"></div>
               <label>Nombre completo</label>
               <div class="input-group input-group-sm mb-3">
-                <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" id="name" name="name" required onchange="verifyName();" placeholder="Digita tu nombre completo">
+                <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" id="name" name="name" required onchange="verifyName();" placeholder="Digita tu nombre completo" maxlength="50">
               </div>
               <div class="alert alert-danger mb-0" role="alert" id="alert_mail" style="display:none;"></div>
               <label>Correo institucional</label>
               <div class="input-group input-group-sm mb-3">
-                <input type="email" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" required id="mail" name="mail" onchange="verifyMail();" placeholder="Ingresa tu correo">
+                <input type="email" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" required id="mail" name="mail" onchange="verifyMail();" placeholder="Ingresa tu correo" maxlength="50">
               </div>
               <label for="program">Programa academico</label>
               <div class="input-group input-group-sm mb-3">
@@ -253,15 +329,58 @@ body {
               <div class="alert alert-danger mb-0" role="alert" id="alert_pw" style="display:none;"><strong>Error!</strong> Las contraseñas no coinciden</div>
               <label>Contraseña</label>
               <div class="input-group input-group-sm mb-3">
-                <input type="password" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" id="pass" name="pass" required onchange="verifyPass();" placeholder="Digita tu contraseña">
+                <input type="password" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" id="pass" name="pass" required onchange="verifyPass();" placeholder="Digita tu contraseña" minlength="6" maxlength="12">
               </div>
               <label>Confirmar Contraseña</label>
               <div class="input-group input-group-sm mb-3">
-                <input type="password" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" id="verify" name="verify" required onchange="verifyPass();" placeholder="Verifica tu contraseña">
+                <input type="password" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" id="verify" name="verify" required onchange="verifyPass();" placeholder="Verifica tu contraseña" maxlength="12">
               </div>
               <center><button type="submit" class="btn btn-success btn-user btn-block">Registrar</button></center>    
             </form>
           </div>
+          <!-- Este es el formulario que se desplega cuando se pulsa empresa -->
+          <div id="form_empresa" name="form_empresa" style="display:none;" enctype="multipart/form-data">
+            <a href="javascript:void(0);" class="btn btn-success" style="float:left;" onclick="formAtWelcome();">Volver</a>
+            <form id="f_emp" action="javascript:void(0);" onsubmit="regCompany();">
+              <center><h3 style="margin-right: 10%;">Formulario de registro Empresas</h3></center><br>
+              <div class="form-group">
+                  <label for="photo">Logo de la empresa:</label>
+                  <input type="file" class="form-control-file dropify" name="logo" id="logo" accept=".png,.jpeg,.jpg" data-allowed-file-extensions="png jpeg jpg" required>
+              </div>
+              <label>Razon social</label>
+              <div class="input-group input-group-sm mb-3">
+                <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" id="nameCp" name="nameCp" required placeholder="Digita la razon social de la empresa" maxlength="50">
+              </div>
+              <label>NIT</label>
+              <div class="input-group input-group-sm mb-3">
+                <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" id="nitCp" name="nitCp" required placeholder="Digita el NIT de la empresa" maxlength="50">
+              </div>
+              <div class="alert alert-danger mb-0" role="alert" id="alert_mailCp" style="display:none;"></div>
+              <label>Correo</label>
+              <div class="input-group input-group-sm mb-3">
+                <input type="email" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" required id="mailCp" name="mailCp" placeholder="Ingresa tu correo" maxlength="50">
+              </div>
+              <label>Descripción</label>
+              <div class="input-group input-group-sm mb-3">
+                <textarea class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" id="desCp" name="desCp" required placeholder="Danos una breve descripción de tu empresa" maxlength="1200"></textarea>
+              </div>
+              <div class="form-group">
+                  <label for="photo">Camara de comercio de la empresa:</label>
+                  <input type="file" class="form-control-file dropify" name="cc" id="cc" accept=".pdf" data-allowed-file-extensions="pdf" required>
+              </div>
+              <div class="alert alert-danger mb-0" role="alert" id="alert_pwCp" style="display:none;"><strong>Error!</strong> Las contraseñas no coinciden</div>
+              <label>Contraseña</label>
+              <div class="input-group input-group-sm mb-3">
+                <input type="password" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" id="passCp" name="passCp" required onchange="verifyPassCp();" placeholder="Digita tu contraseña" minlength="6" maxlength="12">
+              </div>
+              <label>Confirmar Contraseña</label>
+              <div class="input-group input-group-sm mb-3">
+                <input type="password" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" id="verifyCp" name="verifyCp" required onchange="verifyPassCp();" placeholder="Verifica tu contraseña" maxlength="12">
+              </div>
+              <center><button type="submit" class="btn btn-success btn-user btn-block">Registrar</button></center>    
+            </form>
+          </div>
+          <!-- Este div se desplega, para seleccionar si es un estudiante o una empresa -->
           <div id="form_bienvenida" class="text-center">
             <a href="index.php" class="btn btn-warning" style="float:left;">Volver</a><br>
             <h5 class="card-title">Bienvenido al formulario de registro</h5>
@@ -269,7 +388,7 @@ body {
             <center><table>
                 <tr>
                   <td>
-                    <a href="#" class="company"></a>
+                    <a href="javascript:void(0);" onclick="formAtCompany();" class="company"></a>
                   </td>
                   <td>
                     <a href="javascript:void(0);" onclick="formAtStud();" class="student"></a>
@@ -292,7 +411,7 @@ body {
   
 
   <!-- Bootstrap core JavaScript-->
- 
+
 
 </body>
 

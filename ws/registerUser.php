d4b05db4f01b4b490c9ef8b25aaf3afd984a0771
jgrimaldos <albertogrimal2@hotmail.com>
2020-09-02 13:44:38 -0500

    correo 2 v:

--- ws/registerUser.php
+++ ws/registerUser.php
@@ -22,19 +22,29 @@ function enviarCorreo($mail, $name)
     $PHPmail->Subject    = 'Confirma tu correo electrónico';
     
     $PHPmail->AddEmbeddedImage("..\assets\images\logos\logoMail.jpg", "logo");
-    $PHPmail->MsgHTML('<div style="background-color: rgba(222,222,222,0.6); margin-left: 15%; margin-right: 15%;">
-                        <div style="text-align: center;">
-                        <img src=\'cid:logo\' alt="Universidad El Bosque" style=" max-width: 100%; max-height: 100%;
-                        pointer-events: none;
-                        cursor: default;">
-                        </div>
-                        <div style="margin-left: 10%; margin-right: 10%;">
-                        <br>
-                        <p>Te damos la bienvenida '.$name.' al portal de practicas de la Universidad El Bosque.</p>
-                        <p>Para poder usar todos nuestros servicios por favor haz click en el siguiente boton para terminar tu proceso de registro.</p>
-                        <br><br>
-                        </div>
-                        </div>');
+    $PHPmail->AddEmbeddedImage("..\assets\images\logos\footer.png", "footer");
+    $PHPmail->MsgHTML('
+            <div style="background-color: rgba(222,222,222,0.6); margin-left: 15%; margin-right: 15%;">
+    <div style=" margin-left: 5%; margin-right: 5%; padding-top: 5%; padding-bottom: 5%;">
+        <div style="background-color: rgba(255,255,255);">
+            <div style="text-align: center;">
+                <img s src=\'cid:logo\' alt="Universidad El Bosque" style=" max-width: 100%; max-height: 100%;
+                pointer-events: none;
+                cursor: default;">
+            </div>
+            <div style="margin-left: 10%; margin-right: 10%;"><br><br>
+                <p>Te damos la bienvenida '.$name.' al portal de practicas de la Universidad El Bosque.</p>
+                <p>Para poder usar todos nuestros servicios por favor haz click en el siguiente boton para terminar tu proceso de registro.</p><br><br><br>
+                <center><a href="https://practicasuelbosuqe.azurewebsites.net/" style="background-color:#f3984d;border:10px solid #f3984d;text-decoration:none;color:#fff" target="_blank">Activar Cuenta</a></center>
+                <br><br>
+            </div>
+            <br><br><br><br>
+            <center>
+                <img src=\'cid:footer\' alt="footer" style=" max-width: 100%; max-height: 100%;  "></center>
+        </div>
+    </div>
+</div>
+            ');
 
     $PHPmail->AddAddress($mail, $name);
         

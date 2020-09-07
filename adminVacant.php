4d5040e149698f5f202d94362d0a889d04ca620d
pochecito <48699520+pochecito@users.noreply.github.com>
2020-09-06 20:52:44 -0500

    Lista de vacantes

---
+++ adminVacant.php
@@ -0,0 +1,291 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['redirect'])) {
+    header('Location: index.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Home</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="assets/vendors/iconfonts/mdi/css/materialdesignicons.css">
+    <link rel="stylesheet" href="assets/vendors/css/vendor.addons.css">
+    <!-- endinject -->
+    <!-- vendor css for this page -->
+    <!-- End vendor css for this page -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="assets/css/shared/style.css">
+    <!-- endinject -->
+    <!-- Layout style -->
+    <link rel="stylesheet" href="assets/css/demo_1/style.css">
+    <!-- Layout style -->
+    <link rel="shortcut icon" href="assets/images/favicon.ico" />
+  </head>
+
+
+<style>
+.t-header .t-header-brand-wrapper a .logo {
+  width:150px;
+  height:50px;
+}
+.display-avatar.animated-avatar:before {
+  background-color: orange;
+  background-image: linear-gradient(19deg, orange 0%, red 100%)
+}
+.navbar-dropdown .dropdown-body .dropdown-grid {
+  width:40%;
+  margin-left:8%;
+}
+</style>
+<script>
+   window.onload=function(){
+
+    getVacants();
+
+  };
+
+
+  function getVacants(){
+    $.ajax({
+        type: "POST",
+        url: "ws/getVacants.php",
+        success: function (data) {    
+        data = JSON.parse(data);  
+            if (data["status"] == 1) {
+                data = data["vacants"];
+                var html = '';
+                var i;
+                for (i = 0; i < data.length; i++) {
+                  if(data[i]["estado"]=="CERRADO"){
+                    var estado = 'badge badge-danger';
+                  }else if(data[i]["estado"]=="PUBLICADO"){
+                    estado='badge badge-success';
+                  }else{
+                    estado='badge badge-info';
+                  }
+                html += '<tr>' +
+             '<td>' + data[i]["nombre_cargo"] + '</td>' +
+             '<td>' + data[i]["descripcion_vacante"] + '</td>' +
+             '<td>' + data[i]["educacion_base"] + '</td>' +
+             '<td>' + data[i]["horario_disponibilidad"] + '</td>' +
+             '<td>' + data[i]["rango_salarial"] + '</td>' +
+             '<td><div class="'+estado+'">' + data[i]["estado"] + '</div></td>' +
+             '<td>' + data[i]["fecha_vacante"] + '</td>' +
+             '<td>' + data[i]["cod_empresa"] + '</td>';
+           }
+
+          $('#vacant').html(html);
+          
+            }
+        },
+        error: function (data) {
+            console.log(data);
+        },
+    })
+  }
+
+</script>
+
+  <body class="header-fixed">
+    <!-- partial:../partials/_header.html -->
+    <nav class="t-header">
+      <div class="t-header-brand-wrapper">
+        <a href="adminVacant.php">
+          <img class="logo" src="assets/images/logo.png" alt="">
+          <img class="logo-mini" src="assets/images/logo.png" alt="">
+        </a>
+      </div>
+      <div class="t-header-content-wrapper">
+        <div class="t-header-content">
+          <button class="t-header-toggler t-header-mobile-toggler d-block d-lg-none">
+            <i class="mdi mdi-menu"></i>
+          </button>
+          <ul class="nav ml-auto">
+            <li class="nav-item dropdown">
+              <a class="nav-link" href="#" id="notificationDropdown" data-toggle="dropdown" aria-expanded="false">
+                <i class="mdi mdi-bell-outline mdi-1x"></i>
+              </a>
+              <div class="dropdown-menu navbar-dropdown dropdown-menu-right" aria-labelledby="notificationDropdown">
+                <div class="dropdown-header">
+                  <h6 class="dropdown-title">Notifications</h6>
+                  <p class="dropdown-title-text">You have 4 unread notification</p>
+                </div>
+                <div class="dropdown-body">
+                  <div class="dropdown-list">
+                    <div class="icon-wrapper rounded-circle bg-inverse-primary text-primary">
+                      <i class="mdi mdi-alert"></i>
+                    </div>
+                    <div class="content-wrapper">
+                      <small class="name">Storage Full</small>
+                      <small class="content-text">Server storage almost full</small>
+                    </div>
+                  </div>
+                  <div class="dropdown-list">
+                    <div class="icon-wrapper rounded-circle bg-inverse-success text-success">
+                      <i class="mdi mdi-cloud-upload"></i>
+                    </div>
+                    <div class="content-wrapper">
+                      <small class="name">Upload Completed</small>
+                      <small class="content-text">3 Files uploded successfully</small>
+                    </div>
+                  </div>
+                  <div class="dropdown-list">
+                    <div class="icon-wrapper rounded-circle bg-inverse-warning text-warning">
+                      <i class="mdi mdi-security"></i>
+                    </div>
+                    <div class="content-wrapper">
+                      <small class="name">Authentication Required</small>
+                      <small class="content-text">Please verify your password to continue using cloud services</small>
+                    </div>
+                  </div>
+                </div>
+                <div class="dropdown-footer">
+                  <a href="#">View All</a>
+                </div>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link" href="#" id="appsDropdown" data-toggle="dropdown" aria-expanded="false">
+                <i class="mdi mdi-apps mdi-1x"></i>
+              </a>
+              <div class="dropdown-menu navbar-dropdown dropdown-menu-right" aria-labelledby="appsDropdown">
+                <div class="dropdown-header">
+                  <h6 class="dropdown-title">Opciones</h6>
+                </div>
+                <div class="dropdown-body border-top pt-0">
+                  <a class="dropdown-grid">
+                    <i class="grid-icon mdi mdi-security mdi-2x"></i>
+                    <span class="grid-tittle">Cambiar contraseña</span>
+                  </a>
+                  <a class="dropdown-grid" href="logout.php">
+                    <i class="grid-icon mdi mdi-exit-to-app mdi-2x"></i>
+                    <span class="grid-tittle">Cerrar sesión</span>
+                  </a>
+                </div>
+                
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- partial -->
+    <div class="page-body">
+      <!-- partial:../partials/_sidebar.html -->
+      <div class="sidebar">
+        <div class="user-profile">
+          <div class="display-avatar animated-avatar">
+            <img class="profile-img img-lg rounded-circle" src="assets/images/profile/female/image_1.png" alt="profile image">
+          </div>
+          <div class="info-wrapper">
+            <p class="user-name"><?php echo $_SESSION['nombre'];?></p>
+          </div>
+        </div>
+        <ul class="navigation-menu">
+          <li class="nav-category-divider">Menu</li>
+          <li>
+            <a href="adminHome.php">
+              <span class="link-title">Estadisticas</span>
+              <i class="mdi mdi-gauge link-icon"></i>
+            </a>
+          </li>
+          
+          <li>
+            <a href="adminCompany.php">
+              <span class="link-title">Empresas</span>
+              <i class="mdi mdi mdi-bookmark-plus link-icon"></i>
+            </a>
+          </li>
+          <li>
+            <a href="adminStudents.php">
+              <span class="link-title">Estudiantes</span>
+              <i class="mdi mdi mdi-human-greeting link-icon"></i>
+            </a>
+          </li>
+          <li>
+            <a href="adminVacant.php">
+              <span class="link-title">Vacantes</span>
+              <i class="mdi mdi-clipboard-outline link-icon"></i>
+            </a>
+          </li>
+                         
+        </ul>
+      </div>
+      <!-- partial -->
+      <div class="page-content-wrapper">
+        <div class="page-content-wrapper-inner">
+          <div class="viewport-header">
+            <div class="row">
+              <div class="col-12 py-5">
+                <h4>Vacantes</h4>
+              </div>
+            </div>       
+          </div>
+          <div class="content-viewport">
+            <div class="row">              
+              <div class="col-lg-27">
+                <div class="grid">
+                  <p class="grid-header">Lista de Vacantes</p>
+                  <div class="item-wrapper">
+                    <div class="table-responsive">
+                      <div style="width: 1060px;">
+                      <table class="table info-table table-striped">
+                        <thead>
+                          <tr>
+                            <th>Nombre del cargo</th>
+                            <th>Descripción</th>
+                            <th>Educacion base</th>
+                            <th>Horario</th>
+                            <th>Salario</th>
+                            <th>Estado</th>
+                            <th>Fecha</th>
+                            <th>Empresa</th>
+                          </tr>
+                        </thead>
+                        <tbody id="vacant" >
+                                   
+                        </tbody>
+                        
+                      </table>
+                      <div style="width: 1060px;">
+                    </div> 
+                  </div>
+                </div>
+              </div>             
+        <!-- content viewport ends -->
+        <!-- partial:../partials/_footer.html -->
+        <footer class="footer">
+          <div class="row">
+            <div class="col-sm-6 text-center text-sm-right order-sm-1">
+              <ul class="text-gray">
+                <li><a href="#">Terms of use</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+              </ul>
+            </div>
+            <div class="col-sm-6 text-center text-sm-left mt-3 mt-sm-0">
+              
+            </div>
+          </div>
+        </footer>
+        <!-- partial -->
+      </div>
+      <!-- page content ends -->
+    </div>
+
+    <!--page body ends -->
+    <!-- SCRIPT LOADING START FORM HERE /////////////-->
+    <!-- plugins:js -->
+    <script src="assets/vendors/js/core.js"></script>
+    <script src="assets/vendors/js/vendor.addons.js"></script>
+    <!-- endinject -->
+    <!-- Vendor Js For This Page Ends-->
+    <!-- Vendor Js For This Page Ends-->
+    <!-- build:js -->
+    <script src="assets/js/template.js"></script>
+    <!--  -->
+  </body>
+</html>
--- ws/getVacants.php
+++ ws/getVacants.php
@@ -1,59 +1,40 @@
 <?php
 include_once('../persistencia/db.php');
-session_start();
-
-$query = "SELECT * from VACANTE where cod_empresa=".$_SESSION['id'];
+$query = "SELECT nombre_cargo, descripcion_vacante, educacion_base, horario_disponibilidad, rango_salarial, estado, fecha_vacante, cod_empresa from VACANTE";
 
 $stmt = $mysqli->prepare($query);
 $stmt -> execute();
-$stmt -> bind_result($cod_vacante,$nombre_cargo,$descripcion_vacante,$educacion_base,$horario_disponibilidad,$rango_salarial,$estado,$cantidad_vacantes,$fecha_vacante,$cod_empresa);
+$stmt -> bind_result($nombre_cargo,$descripcion_vacante,$educacion_base,$horario_disponibilidad,$rango_salarial,$estado,
+    $fecha_vacante,$cod_empresa);
 
 $rta="";
 $vacants=array();
-$html=" <div class='row' style='float:right; margin-bottom:5%; margin-right:5%'>
-            <a class='btn btn-warning' href='javascript:void(0)' onclick='openModal();'>Publicar</a>
-        </div>
-        <div class='row' style='margin-top:6%;'>";
-$aux=0;
-$entro=false;
 while($stmt -> fetch()) {
-    $entro=true;
-    $aux=$aux+1;
-    $margin=($aux==1) ? '' : 'margin-left:5%;';
-    $aux=($aux==3) ? 0 : $aux;
-    $html.='<div class="card" style="width: 30%; '.$margin.' margin-top:2%; background-image: linear-gradient(120deg,#00e795 0,#0095e2 100%);
-    ">
-                <div class="card-body" style="color:#fff">
-                    <h4 class="text-center">'.$nombre_cargo.'</h4><br>
-                    <h6 class="card-subtitle mb-2 text-center" >Cantidad Vacantes: '.$cantidad_vacantes.'</h6>
-                    <center><label class="badge badge-dark">'.$estado.'</label></center>
-                    <p class="card-text">
-                        <strong>Rango salarial:</strong> '.$rango_salarial.'<br>
-                        <strong>Horario:</strong> '.$horario_disponibilidad.'<br>
-                        <strong>Fecha:</strong> '.$fecha_vacante.'<br>
-                        <strong>Descripción:</strong> '.$descripcion_vacante.'<br>
-                        <strong>Educación base:</strong> '.$educacion_base.'<br>
-                    </p>
-                </div>
-            </div>';
+    $aux=1;
+    $vacant=array(
+        "nombre_cargo"=>$nombre_cargo,
+        "descripcion_vacante"=>$descripcion_vacante,
+        "educacion_base"=>$educacion_base,
+        "horario_disponibilidad"=>$horario_disponibilidad,
+        "rango_salarial"=>$rango_salarial,
+        "estado"=>$estado,
+        "fecha_vacante"=>$fecha_vacante,
+        "cod_empresa"=>$cod_empresa
+    );
+    array_push($vacants,$vacant);
 }
 $response=array();
-if($entro){
+if(count($vacants)>0){
     $response = array(
-        'html' => $html.'</div>',
+        'vacants' => $vacants,
         'status' => 1
     );
 }else{
     $response = array(
-        'html' => "<div style='margin-top:3%;margin-left:15%;'>
-                        <h1>Aun no has publicado ninguna vacante </h1><br>
-                        <img src='assets/images/comencemos.png' width='600px' height='500px'>
-                        <a class='btn btn-warning btn-lg' style='color:white;' href='javascript:void(0)' onclick='openModal();'>Comencemos!!</a>
-                   </div>",
-        'status' => 1
+        'html' => "error",
+        'status' => 0
     );
 }
 $stmt->close();
-
 echo json_encode($response);
 ?>

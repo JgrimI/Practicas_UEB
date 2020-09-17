271d23458afd7330a3323c7bac08c373fbcbb34d
jleonf2020 <61398900+jleonf2020@users.noreply.github.com>
2020-09-17 03:15:42 -0500

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
     $google_client = new Google_Client();
     $google_client->setClientId('158567667651-kf38js0b6f7rb7u3s51eu2amgqs5jrqh.apps.googleusercontent.com');
     $google_client->setClientSecret('vAev39POfSodoZLqYIIeZlA1');
-    $google_client->setRedirectUri('http://localhost:8082/Practicas_UEB/index.php');
+    $google_client->setRedirectUri('https://practicasueb.azurewebsites.net/index.php');
     $google_client->addScope('email');
     $google_client->addScope('profile');
 ?>

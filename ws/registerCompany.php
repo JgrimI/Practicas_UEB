77b9d6172b8c62cc2b47c62c6ef0539664652f49
jgrimaldos <albertogrimal2@hotmail.com>
2020-09-17 05:07:38 -0500

    mensajes correos

--- ws/registerCompany.php
+++ ws/registerCompany.php
@@ -24,28 +24,25 @@ function enviarCorreo($mail, $name)
    $PHPmail->SetFrom('practicas.uelbosque@gmail.com', "Practicas UEB");
    $PHPmail->Subject    = 'Confirma tu correo electrónico';
    
-   $PHPmail->AddEmbeddedImage("..\assets\images\logos\logoMail.png", "logo");
-
-   $PHPmail->AddEmbeddedImage("..\assets\images\logos\logoMail.jpg", "logo");
-   $PHPmail->AddEmbeddedImage("..\assets\images\logos\footer.png", "footer");
+   $PHPmail->AddEmbeddedImage('../assets/images/logos/logoMail.png', 'logoMail');
+   $PHPmail->AddEmbeddedImage('../assets/images/logos/footer.png', 'logofooter');
    $PHPmail->MsgHTML('
            <div style="background-color: rgba(222,222,222,0.6); margin-left: 15%; margin-right: 15%;">
             <div style=" margin-left: 5%; margin-right: 5%; padding-top: 5%; padding-bottom: 5%;">
                 <div style="background-color: rgba(255,255,255);">
                     <div style="text-align: center;">
-                        <img s src=\'cid:logo\' alt="Universidad El Bosque" style=" max-width: 100%; max-height: 100%;
-                        pointer-events: none;
+                    <img src=\'cid:logoMail\' alt="Universidad El Bosque" style=" max-width: 100%; max-height: 100%;
+                    pointer-events: none;
                         cursor: default;">
                     </div>
                     <div style="margin-left: 10%; margin-right: 10%;"><br><br>
                         <p>Te damos la bienvenida '.$name.' al portal de practicas de la Universidad El Bosque.</p>
-                        <p>Para poder usar todos nuestros servicios por favor haz click en el siguiente boton para terminar tu proceso de registro.</p><br><br><br>
-                        <center><a href="https://practicasuelbosuqe.azurewebsites.net/" style="background-color:#f3984d;border:10px solid #f3984d;text-decoration:none;color:#fff" target="_blank">Activar Cuenta</a></center>
-                        <br><br>
+                        <p>En este momento te encuentras en proceso de espera.</p><br><br><br>
+                       <br><br>
                     </div>
                     <br><br><br><br>
                     <center>
-                        <img src=\'cid:footer\' alt="footer" style=" max-width: 100%; max-height: 100%;  "></center>
+                    <img src=\'cid:logofooter\' alt="footer" style=" max-width: 100%; max-height: 100%;  "></center>
                     </div>
                 </div>
             </div>
